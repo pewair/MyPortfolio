a47b339e873c16647fada8d82016c269c3ac3830
pewair <gourangasinha07@gmail.com>
2023-11-27 17:41:10 +0530

    27/11/2023 cut the intro shit and separate pt1

---
+++ intro.js
@@ -0,0 +1,31 @@
+const fonts = [
+    'Kirang Haerang',
+    'Indie Flower',
+    'Rye',
+    'Amatic SC',
+    'Bangers',
+    'Fredericka the Great'
+  ];
+  const letters = document.querySelectorAll('.letter');
+  let count=0;
+  
+  const rollIntro = () => {
+    letters.forEach(letter => {
+      
+      let randomFontIndex = Math.floor(Math.random() * fonts.length);
+      let randomFont = fonts[randomFontIndex];
+      letter.style.fontFamily=randomFont;
+    });
+  }
+  let introAnimation = setInterval(function() {
+    rollIntro();
+    if(count>15)
+      clearInterval(introAnimation);
+    count++;
+  },350);
+
+  var delayInMilliseconds = 8000;
+
+setTimeout(function() {
+  window.location.href = "index.htm";
+}, delayInMilliseconds);
